d5bd1d06fd9cf356c1faa67a034c8ec4568d05a9
bykcss <business.sorii@gmail.com>
2025-05-16 03:23:52 -0600

    Add files via upload

---
+++ frases_aleatorias.js
@@ -0,0 +1,16 @@
+const frasesAleatorias = [
+  "eres fuerte y de corazón noble, pero te duele perder a quien amabas y ver que siguió con su vida, la canción que más te representa es: “hacia otro camino” (Track 2)",
+  "amas con todas tus fuerzas y eso es lo que más te representa, temes perder a quien te acompaña, la canción que más te representa es: “xfa no te vayas” (Track 3)",
+  "eres libre, y, aunque amas con todo el corazón, no te rompes fácil y es difícil que te manipulen, la canción que más te representa es: “que te vaya bn” (Track 4)",
+  "amas y eres leal, el echo de perder a alguien desencadena que te pierdas también, la canción que más te representa es “nada de love” (Track 5)",
+  "sabes que el final estaba cerca y lo aceptaste, aunque te duele, prefieres salir de ese momento oscuro, la canción que más te representa es “me arrepentiré” (Track 6)",
+  "la nostalgia te juega siempre en contra, y aunque tu mente aceptó que se acabó, tu corazón grita que no termine, la canción que más te representa es “solo suéltame” (Track 7)",
+  "aceptaste perder, te duele, y no sabes cómo seguir adelante, intentas buscar donde no hay pero sabes que puede haber una salida, la canción que más te representa es “es mejor así” (Track 9)",
+  "tu intuición no falla; sabes que todo está mal pero dentro tienes la esperanza de que algo cambie, la canción que más te representa es “no te importó” (Track 10)",
+  "amas con todo el corazón pero sabes que no es correspondido, la canción que más te representa es “desconocidos” (Track 11)",
+  "sigues buscando a la persona que cambió tu forma de amar, quién realmente te hizo feliz, aunque sabes que no es el camino, la canción que más te representa es “interlude - como te recordaré” (Track 12)",
+  "amas, sueñas y luchas, pero sabes que tu relación no da para más, te estás dando cuenta que quizás alejarte es lo mejor aunque duela, la canción que más te representa es “al pendiente” (Track 13)",
+  "eres frágil, pero la persona más linda y con sentimientos puros, tienes miedo del daño pero eres fuerte para seguir adelante, la canción que más te representa es “no lo entienden” (Track 14)",
+  "culpas al mundo pero sabes en el fondo que tu mente te crea los problemas, te estás dando cuenta que quizás el resto no tenía tanta culpa, la canción que más te representa es “debo cambiar” (Track 15)",
+  "eras apegado, dependías emocionalmente de tu pareja, pero aprendiste a seguir adelante y a que no te de miedo la soledad, la canción que más te representa es “esynma” (Track 16)"
+];
